69bffa98eb7df2873323025425e4ce9b2195fffc
Rafael Derolez <raf@derolez.dev>
2021-06-24 20:20:12 +0200

    Update

---
+++ scripts/dock.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#============================================================================
+# Title: Dock
+# Usage: sh dock.sh
+#
+# Description:
+# Sets up dock items according to config.json
+#============================================================================
+
+source ${DOTFILES}/scripts/utils.sh
+
+remove_all_apps() {
+  process "Removing current dock items"
+  dockutil --remove all --no-restart
+}
+
+# Add apps from config, in order of app priority per space
+add_apps() {
+  while IFS=',' read -r app; do
+    local name=$(jq -r '.' <<<"$app")
+    process "Adding application $name"
+    dockutil --add "/Applications/$name.app" --no-restart
+  done <<EOF
+$(jq -c '.dock[]' <"$DOTFILES/config.json")
+EOF
+}
+
+# Add folders with custom icons
+add_folders() {
+  while IFS=',' read -r folder; do
+    local name=$(jq -r '.name' <<<"$folder")
+    local view=$(jq -r '.view' <<<"$folder")
+    local sort=$(jq -r '.sort' <<<"$folder")
+    local icon=$(jq -r '.icon' <<<"$folder")
+    process "Adding folder $name"
+    dockutil --add "$HOME/$name" --view $view --display folder --sort $sort --no-restart
+    process "Setting $name icon"
+    # fileicon -q set "$HOME/$name" "$DOTFILES/icons/$icon"
+  done <<EOF
+$(jq -c '.dockFolders[]' <"$DOTFILES/config.json")
+EOF
+}
+
+restart_dock() {
+  process "Restarting dock"
+  killall Dock
+}
+
+main() {
+  info "Setting up dock"
+  remove_all_apps
+  add_apps
+  add_folders
+  restart_dock
+  success "Dock set up"
+}
+
+main
---
+++ scripts/utils.sh
@@ -0,0 +1,77 @@
+#!/bin/zsh
+#============================================================================
+# Title: Utils
+# Usage: source ${DOTFILES}/scripts/utils.sh
+#
+# Description:
+# Use this at the top of your scripts to utilize colors, formatting and
+# logging
+#============================================================================
+
+# Colors
+GRAY='\033[1;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+PINK='\033[1;35m'
+CYAN='\033[0;36m'
+WHITE='\033[1;37m'
+RESET='\033[0m'
+
+# Decorators
+BOLD='\033[1m'
+NORMAL='\033[2m'
+
+log() {
+  # If we are running in the foreground, set formatting
+  case $(ps -o stat= -p $$) in
+  *+*) printf "$1" "$2" ;;
+  # Otherwise just log!
+  *) echo "$2" ;;
+  esac
+}
+
+alt() {
+  log "${MAGENTA}%s${RESET}\n" "$@"
+}
+
+info() {
+  echo
+  log "${BLUE}${BOLD}%s:${NORMAL}${RESET} \n" "$@"
+}
+
+warn() {
+  log "${YELLOW}⚠ Warning:${RESET} %s!\n" "$@"
+}
+
+error() {
+  log "${RED}⊘ Error:${RESET} %s!\n" "$@"
+}
+
+confirm() {
+  printf "\n"
+  log "\n${YELLOW}==> %s${RESET} " "$@"
+  read -p "Continue? (y/n) " -n 1 -r
+  printf "\n"
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+  fi
+}
+
+prompt() {
+  log "${YELLOW}%s:${RESET} " "$@"
+}
+
+process_title() {
+  log "${PINK}┃ %s${RESET}\n" "$@"
+}
+
+process() {
+  log "${PINK}┃${RESET} %s...\n" "$@"
+}
+
+success() {
+  log "${GREEN}✓ Success:${RESET} %s\n" "$@"
+}
