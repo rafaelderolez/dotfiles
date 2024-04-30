4328d8fb0fdf5104d24004b83da7ed78ea914233
Rafael Derolez <raf@derolez.dev>
2024-04-30 22:24:47 +0200

    Misc wezterm stuff

--- configs/wezterm/misc/utils.lua
+++ configs/wezterm/misc/utils.lua
@@ -1,23 +1,22 @@
-local wezterm = require("wezterm")
-local process_icons = require("misc/icons")
+local wezterm = require "wezterm"
+local process_icons = require "misc/icons"
 
 local M = {}
 
 function M.get_process(tab)
-	local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
+	-- Check if the foreground_process_name is a string before processing
+	local process_name = tab.active_pane.foreground_process_name
 
-	return wezterm.format((process_icons[process_name] or { { Text = string.format("%s:", process_name) } }))
-end
-
-function M.get_current_working_dir(tab)
-	local current_dir = tab.active_pane.current_working_dir
-	local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
-
-	if current_dir == HOME_DIR then
-		return "~ "
+	if type(process_name) == "string" then
+		-- Only use string.gsub if process_name is a string
+		process_name = string.gsub(process_name, "(.*[/\\])(.*)", "%2")
+	else
+		-- If not a string, default to a placeholder process name
+		process_name = "unknown_process"
 	end
 
-	return string.gsub(current_dir, "(.*[/\\])(.*)", "%2")
+	-- Return the formatted process name with icons or as plain text
+	return wezterm.format((process_icons[process_name] or { { Text = string.format("%s:", process_name) } }))
 end
 
 function M.tab_title(tab_info)
@@ -27,19 +26,19 @@ function M.tab_title(tab_info)
 	-- Check if the tab title is explicitly set
 	if not title or #title == 0 then
 		-- If not, use the title from the active pane in that tab
-		title = M.get_current_working_dir(tab_info)
+		title = tab_info.active_pane.title
 	end
+
 	-- Format and return the final tab title
-	return wezterm.format({
+	return wezterm.format {
 		{ Text = "  " },
 		-- { Foreground = { AnsiColor = "Blue" } },
 		{ Text = icon },
 		{ Text = "  " },
 		-- { Foreground = { AnsiColor = "Silver" } },
 		{ Text = title },
-		{ Text = "  " },
 		"ResetAttributes",
-	})
+	}
 end
 
 return M
--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,17 +1,18 @@
-local wezterm = require("wezterm")
+local wezterm = require "wezterm"
 local act = wezterm.action
-local cosmosGUI = require("gui/cosmos")
-local finderopvangGUI = require("gui/finderopvang")
-local timelineGUI = require("gui/timeline")
-local utils = require("misc/utils")
+local cosmosGUI = require "gui/cosmos"
+local finderopvangGUI = require "gui/finderopvang"
+local portfolioGUI = require "gui/portfolio"
+local timelineGUI = require "gui/timeline"
+local utils = require "misc/utils"
 
 local config = wezterm.config_builder()
 
-local monospace = wezterm.font_with_fallback({
+local monospace = wezterm.font_with_fallback {
 	-- "Rec Mono Linear",
 	"Geist Mono",
 	{ family = "Symbols Nerd Font Mono", scale = 0.8 },
-})
+}
 
 -- Font
 config.font = monospace
@@ -19,6 +20,14 @@ config.font_size = 18
 config.line_height = 1.3
 config.use_cap_height_to_scale_fallback_fonts = true
 config.adjust_window_size_when_changing_font_size = false
+config.command_palette_font_size = 18
+config.command_palette_rows = 16
+
+config.command_palette_bg_color = "#15161F"
+config.command_palette_fg_color = "#fff"
+config.window_frame = {
+	-- font = monospace,
+}
 
 -- UI
 config.color_scheme = "Tokyo Night"
@@ -27,8 +36,8 @@ config.window_decorations = "RESIZE"
 config.tab_max_width = 128
 config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = false
--- config.window_background_opacity = 0.95
--- config.macos_window_background_blur = 20
+config.window_background_opacity = 0.94
+config.macos_window_background_blur = 90
 config.window_padding = {
 	left = 0,
 	right = 0,
@@ -47,57 +56,71 @@ config.keys = {
 	{ key = "Tab", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
 	{ key = "Tab", mods = "CTRL|SHIFT", action = wezterm.action.DisableDefaultAssignment },
 
-	{ key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
-	-- Toggle current pane visibility
-	{
-		key = "t",
-		mods = "CTRL",
-		action = wezterm.action.TogglePaneZoomState,
-	},
+	{ key = "w", mods = "LEADER", action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
 
 	-- Pane Actions
 	{
 		key = "\\",
 		mods = "CTRL",
-		action = wezterm.action.SplitPane({
+		action = wezterm.action.SplitPane {
 			direction = "Down",
 			size = { Percent = 20 },
-		}),
+		},
 	},
 	{
 		key = "|",
 		mods = "CTRL|SHIFT",
-		action = wezterm.action.SplitPane({
+		action = wezterm.action.SplitPane {
 			direction = "Right",
 			size = { Percent = 50 },
-		}),
+		},
 	},
 	{
 		key = "w",
 		mods = "CMD",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+		action = wezterm.action.CloseCurrentPane { confirm = true },
+	},
+
+	-- Toggle current pane visibility
+	{
+		key = "t",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(_, pane)
+			local tab = pane:tab()
+			local panes = tab:panes_with_info()
+			if #panes == 1 then
+				pane:split {
+					direction = "Bottom",
+					size = 0.3,
+				}
+			elseif not panes[1].is_zoomed then
+				panes[1].pane:activate()
+				tab:set_zoomed(true)
+			elseif panes[1].is_zoomed then
+				tab:set_zoomed(false)
+				panes[2].pane:activate()
+			end
+		end),
 	},
 
 	{
 		key = "N",
 		mods = "CTRL|SHIFT",
-		action = act.PromptInputLine({
-			description = wezterm.format({
+		action = act.PromptInputLine {
+			description = wezterm.format {
 				{ Attribute = { Intensity = "Bold" } },
 				{ Foreground = { AnsiColor = "Yellow" } },
 				{ Text = "Enter name for new workspace" },
-			}),
+			},
 			action = wezterm.action_callback(function(window, pane, line)
-				if line then
-					window:perform_action(
-						act.SwitchToWorkspace({
-							name = line,
-						}),
-						pane
-					)
-				end
+				if line then window:perform_action(
+					act.SwitchToWorkspace {
+						name = line,
+					},
+					pane
+				) end
 			end),
-		}),
+		},
 	},
 }
 
@@ -115,17 +138,12 @@ config.unix_domains = {
 
 -- Workspaces
 wezterm.on("gui-startup", function()
-	local project = os.getenv("WZ_PROJECT")
+	local project = os.getenv "WZ_PROJECT"
 
-	if project == "timeline" then
-		timelineGUI.setup()
-	end
-	if project == "cosmos" then
-		cosmosGUI.setup()
-	end
-	if project == "finderopvang" then
-		finderopvangGUI.setup()
-	end
+	if project == "timeline" then timelineGUI.setup() end
+	if project == "cosmos" then cosmosGUI.setup() end
+	if project == "finderopvang" then finderopvangGUI.setup() end
+	if project == "portfolio" then portfolioGUI.setup() end
 end)
 
 -- Decorate top-right status bar
@@ -134,17 +152,18 @@ wezterm.on("update-right-status", function(window, pane)
 	local stat = window:active_workspace()
 
 	-- Let's add color to one of the components
-	window:set_right_status(wezterm.format({
+	window:set_right_status(wezterm.format {
 		{ Foreground = { AnsiColor = "Fuchsia" } },
 		{ Text = wezterm.nerdfonts.oct_table .. " " .. stat },
 		"ResetAttributes",
-	}))
+	})
 end)
 
 wezterm.on("format-tab-title", function(tab)
 	local title = utils.tab_title(tab)
 	return {
 		{ Text = title },
+		{ Text = " " },
 	}
 end)
 
