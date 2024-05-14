b31af5b069d299ae13fa8508c5ab4ab18f24d6e4
Rafael Derolez <raf@derolez.dev>
2024-05-14 22:33:02 +0200

    Formatting

--- configs/wezterm/gui/timeline.lua
+++ configs/wezterm/gui/timeline.lua
@@ -1,4 +1,4 @@
-local wezterm = require("wezterm")
+local wezterm = require "wezterm"
 local mux = wezterm.mux
 
 local M = {}
@@ -6,16 +6,16 @@ local M = {}
 local function setup_tab(window, workspaceName, title, config)
 	local tab, terminal
 	if window then
-		tab, terminal = window:spawn_tab({ workspace = workspaceName, cwd = config.dir })
+		tab, terminal = window:spawn_tab { workspace = workspaceName, cwd = config.dir }
 	else
-		tab, terminal, window = mux.spawn_window({ workspace = workspaceName, cwd = config.dir })
+		tab, terminal, window = mux.spawn_window { workspace = workspaceName, cwd = config.dir }
 	end
 
-	local editor = terminal:split({
+	local editor = terminal:split {
 		direction = "Top",
 		size = 0.9,
 		cwd = config.dir,
-	})
+	}
 	tab:set_title(title)
 	terminal:send_text(config.terminal_command .. "\n")
 	editor:send_text(config.editor_command .. "\n")
@@ -41,14 +41,14 @@ function M.setup()
 			},
 		},
 
-		{
-			title = "Wordle",
-			config = {
-				dir = wezterm.home_dir .. "/Code/amazentis/timeline-wordle",
-				terminal_command = "nr dev",
-				editor_command = "nvim",
-			},
-		},
+		-- {
+		-- 	title = "Wordle",
+		-- 	config = {
+		-- 		dir = wezterm.home_dir .. "/Code/amazentis/timeline-wordle",
+		-- 		terminal_command = "nr dev",
+		-- 		editor_command = "nvim",
+		-- 	},
+		-- },
 		{
 			title = "Components",
 			config = {
