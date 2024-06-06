1b2b3cea135a8e8e26cb2795a4f44fcd258741cd
Rafael Derolez <raf@derolez.dev>
2024-06-06 13:25:09 +0200

    Update homebrew.sh

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -2,7 +2,7 @@ chirp --process-title "Homebrew"
 
 if ! command -v brew >/dev/null; then
 	chirp --info "Installing Homebrew"
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	chirp --success "Homebrew installed successfully"
 else
 	chirp --success "Homebrew already installed"
