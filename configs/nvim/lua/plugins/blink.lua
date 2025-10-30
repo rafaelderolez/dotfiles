a5a910fd9f6b447944fbaaa5b32e6d2c34acf478
Rafael Derolez <raf@derolez.dev>
2025-10-30 15:16:42 +0100

    Blink UI opts

---
+++ configs/nvim/lua/plugins/blink.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        menu = {
+          border = "rounded",
+        },
+        documentation = {
+          window = {
+            border = "rounded",
+          },
+        },
+        list = {
+          selection = {
+            preselect = false,
+          },
+        },
+        ghost_text = {
+          enabled = false,
+        },
+      },
+    },
+  },
+}
